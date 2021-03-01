c9e179f1cd2c4bae6b569dd4cf2ec0dc24a1dd94
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-03-02 10:40:34 +1100

    Setup initial Spring Boot skeleton.

---
+++ src/main/java/com/somecompany/Oauth2DemoResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.somecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2DemoResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2DemoResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somecompany/Oauth2DemoResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2DemoResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
