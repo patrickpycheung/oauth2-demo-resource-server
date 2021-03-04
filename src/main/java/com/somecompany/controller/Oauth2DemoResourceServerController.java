836a133ec52780719176553a8157717d562a7409
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-03-04 20:55:14 +1100

    Configure CORS.

--- src/main/java/com/somecompany/controller/Oauth2DemoResourceServerController.java
+++ src/main/java/com/somecompany/controller/Oauth2DemoResourceServerController.java
@@ -1,5 +1,6 @@
 package com.somecompany.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Oauth2DemoResourceServerController {
 
 	@GetMapping("/hello")
+	@CrossOrigin
 	public String api() {
 		return "Made it to protected api on resource server!";
 	}
